524c5d70bff12c444eafdadb9dc5bbf807c18e5e
BeepBoop486 <sustix123@gmail.com>
2019-11-08 18:14:32 -0500

    This is the formula i've used

--- utils/getylimit.js
+++ utils/getylimit.js
@@ -1,4 +1,23 @@
-ty = 61 * 50
-ty2 = 61 * 13
-ylimit = ty2+((ty-ty2)/2)
-console.log(ylimit)
+var ty, ty2, tya;
+var ylimit, g1y, g2y;
+
+for(i = 0; i < 4; i++)
+{
+	if ty < 4800 - 170
+	{
+		ty+=61
+	}
+}
+ty2=ty
+for(i = 0; i < 9; i++)
+{
+	ty2-=61
+}
+tya2=ty2
+for(i = 0; i < 2; i++)
+{
+	tya2-=61
+}
+ylimit=ty2+((ty-ty2)/2)
+g1y=ty
+g2y=tya2
