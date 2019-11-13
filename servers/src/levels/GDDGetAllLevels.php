540e4a61104594dcfb96c4df1edf9e03198145b6
Mya <sustix123@gmail.com>
2019-11-12 19:35:27 -0500

    Added downloads, likes and dislikes to the DB

--- servers/src/levels/GDDGetAllLevels.php
+++ servers/src/levels/GDDGetAllLevels.php
@@ -9,6 +9,9 @@
 			echo $row["id"] . "\n";
 			echo $row["rate"] . "\n";
 			echo $row["stars"] . "\n";
+			echo $row["downloads"] . "\n";
+			echo $row["likes"] . "\n";
+			echo $row["dislikes"] . "\n";
 		}
 	}
 
