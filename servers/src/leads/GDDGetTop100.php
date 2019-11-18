2b42028dec2b5dcfe36b00260b45f6fb640be6e8
Mya <sustix123@gmail.com>
2019-11-17 19:36:31 -0500

    Added a very basic leaderboard system
    The top will be updated each time a user saves in the cloud

---
+++ servers/src/leads/GDDGetTop100.php
@@ -0,0 +1,28 @@
+<?php 
+
+	include '../inc/db.php';
+
+	$query = $conn->query("SELECT * FROM users ORDER BY stars DESC LIMIT 100");
+	if($query)
+	{
+		if ($query->num_rows > 0) {
+			while($row = $query->fetch_assoc()) {
+				echo $row["uname"] . "\n";
+				echo $row["stars"] . "\n";
+				echo $row["diamonds"] . "\n";
+				echo $row["coins"] . "\n";
+				echo $row["userCoins"] . "\n";
+				echo $row["demons"] . "\n";
+				echo $row["creatorPoints"] . "\n";
+				echo $row["icon"] . "\n";
+				echo $row["color1"] . "\n";
+				echo $row["color2"] . "\n";
+			}
+		} else {
+			echo "There're not players";
+		}
+	} else {
+		echo "Error querying in the server";
+	}
+
+ ?>
